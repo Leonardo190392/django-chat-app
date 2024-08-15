8f31fd622266892efe13113dd145b999a09c31df
Leonardo van Dahl <111518533+Leonardo190392@users.noreply.github.com>
2024-08-15 14:08:44 +0200

    frist steps

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,10 @@
+from .models import Message
 from django.contrib import admin
 
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('text', 'created_at', 'author', 'receiver') # zeigt die genannten sachen im einzelnen object an
+    list_display = ('text', 'created_at', 'author', 'receiver') # zeigt die genannten Sachen im Overlay(Chat) an
+    search_fields = ('text',) # so kann man nach text suchen/filtern
+
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.fields import DateField
+from datetime import date
+from django.conf import settings
 
 # Create your models here.
+class Message(models.Model):
+    text = models.CharField(max_length=500) # max_length = maximale Zeichen
+    created_at = DateField(default = date.today) # aktuelles datum
+    # chat = Chat KLasse Verknüpfen
+    author  = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
