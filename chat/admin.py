8abc5750cfd5a51a6ff1152501fff73546fe1ddf
Leonardo van Dahl <111518533+Leonardo190392@users.noreply.github.com>
2024-08-16 09:54:35 +0200

    login update

--- chat/admin.py
+++ chat/admin.py
@@ -1,10 +1,11 @@
-from .models import Message
+from .models import Message, Chat
 from django.contrib import admin
 
 class MessageAdmin(admin.ModelAdmin):
-    fields = ('text', 'created_at', 'author', 'receiver') # zeigt die genannten sachen im einzelnen object an
+    fields = ('chat','text', 'created_at', 'author', 'receiver') # zeigt die genannten sachen im einzelnen object an
     list_display = ('text', 'created_at', 'author', 'receiver') # zeigt die genannten Sachen im Overlay(Chat) an
     search_fields = ('text',) # so kann man nach text suchen/filtern
 
 # Register your models here.
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,23 @@
+
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from .models import Chat, Message
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def index(request):
-    return render(request, 'chat/index.html', {'username': 'Junus'}) # Variable an die Index.html weitergegeben
+    if request.method == 'POST': # die funktion wird nur ausgeführt, wenn man ein POST reqeust ausführt
+        print("Received data" + request.POST['textmessage']) # ist die variable aus der index html des inputs
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user) # erstellt eine neue Nachricht
+    chatMessages = Message.objects.filter(chat__id=1)  # wir holen die nachrichten wo die chat id 1 ist   
+    return render(request, 'chat/index.html', {'messages': chatMessages }) # Variable an die Index.html weitergegeben
+
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get('username') , password=request.POST.get('password'))
+        if user:
+            return HttpResponseRedirect('/chat/')
+        else:
+            return render(request, 'auth/login.html', {'wrongPassword': True} )
+    return render(request, 'auth/login.html')
